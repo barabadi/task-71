a5ed33e77f6e900cb5def0c7efc8d4850df896f9
barabadi <101886037+barabadi@users.noreply.github.com>
2023-02-06 17:54:00 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import "./App.css";
+import React, { useState, useEffect } from 'react';
+
+const Document = ({ title, content, handleScroll }) => {
+  return (
+    <div>
+      <h2 className="title">{title}</h2>
+      <div className="content" onScroll={handleScroll}>
+        {content}
+      </div>
+    </div>
+  );
+};
 
 function App() {
+  const [content, setContent] = useState('');
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+
+  useEffect(() => {
+    fetch('https://jaspervdj.be/lorem-markdownum/markdown.txt')
+      .then(response => response.text())
+      .then(text => {
+        setContent(text);
+      });
+  }, []);
+
+  const handleScroll = event => {
+    if (event.target.scrollTop + event.target.clientHeight >= event.target.scrollHeight) {
+      setIsButtonEnabled(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
-      </div>
+    <div>
+      <Document title="Terms and Conditions" content={content} handleScroll={handleScroll} />
+      <button disabled={!isButtonEnabled}>I Agree</button>
     </div>
   );
 }
 
 export default App;
+
